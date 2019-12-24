4f9133510e5a40ac6c218619e057326deecbb324
UrUserName <acfun@boger.com>
2019-12-24 23:35:10 +0800

    Add requests module

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import urllib3
 import os 
+import requests
+
 myid = os.getenv("TARGET_CHAT_ID")
 bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
-send_text = "send text ...."
-sendMSGAPI = rf"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={myid}&text={send_text}"
-http = urllib3.PoolManager()
-rsp = http.request('GET', sendMSGAPI) 
-print(rsp.status)
-# print(rsp.data)
+def sendText(msg):
+    send_text = msg.strip()
+    sendMSGAPI = rf"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={myid}&text={send_text}"
+    http = urllib3.PoolManager()
+    rsp = http.request('GET', sendMSGAPI) 
+    print(rsp.status)
+    # print(rsp.data)
+
+def sendText2(msg):
+    send_text = msg.strip()
+    sendMSGAPI = rf"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={myid}&text={send_text}"
+    r = requests.get(sendMSGAPI)
+    print(r.status_code)    
+
+
+if __name__ == '__main__':
+    sendText("In function....")
+    sendText2("Use requests....")
