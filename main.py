f5820fe761a53b872d7c3df3f166a7314d186aeb
UrUserName <acfun@boger.com>
2020-02-09 18:02:51 +0800

    Media file send as File

--- main.py
+++ main.py
@@ -71,15 +71,20 @@ if __name__ == '__main__':
     #sendText2("https://www.google.com")
     li = get_img_list()
 
-    sendText2(f"Action start....,get len is {len(li)}")
+    # #sendText2(f"Action start....,get len is {len(li)}")
     
     if len(li) != 0:
         for each in li:
+            if each is not None and "." in each:
+                sub_ext = each.split(".")[1]
+            else:
+                sub_ext = None 
+
             if each is not None:
-                if sendPhoto(each.strip()):
+                if sub_ext not in ["mp4","gif","webm"] and sendPhoto(each.strip()):
                     pass
                 else:
                     sendFile(each.strip())   # sendText2(each.strip())
                 time.sleep(1)
-                
-    sendText2(f"Action Finish, {time.ctime()}")
+    sendText2(f"Get len is {len(li)} at {time.ctime()}")               
+    # #sendText2(f"Action Finish, {time.ctime()}")
